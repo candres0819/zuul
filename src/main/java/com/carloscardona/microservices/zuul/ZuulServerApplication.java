38a7c415b966d250f012c3dc5a0702bb3ebc442a
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-06-08 22:55:02 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

--- src/main/java/com/carloscardona/microservices/zuul/ZuulServerApplication.java
+++ src/main/java/com/carloscardona/microservices/zuul/ZuulServerApplication.java
@@ -1,17 +1,11 @@
 package com.carloscardona.microservices.zuul;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Controller
 @EnableZuulProxy
+@SpringBootApplication
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
